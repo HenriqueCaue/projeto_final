5014d3fb013035aacd40513c66c69a725e69fc48
Henrique Cauê <henrique_caue@hotmail.com>
2020-05-20 03:01:10 -0300

    Visitante entra na página e tenta aplicar pra uma vaga

--- spec/features/visitor_open_homepage_spec.rb
+++ spec/features/visitor_open_homepage_spec.rb
@@ -6,4 +6,15 @@ feature 'Visitor open homepage' do
 
     expect(page).to have_content('Bem vindo ao sistema de vagas de emprego')
   end
+
+  scenario 'and try to apply' do
+    job = create(:job_opportunity)
+
+    visit root_path
+    click_on job.title
+
+    click_on 'Candidatar-se'
+
+    expect(current_path).to eq new_candidate_session_path
+  end
 end
